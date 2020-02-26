bd60fc1e0726846d750fb8396dad11cc1131f074
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-02-26 20:07:50 +0100

    MostFrequentWords added

---
+++ MostFrequentWords/MostFrequentWords.py
@@ -0,0 +1,23 @@
+wordsList = []
+wordsDictionary = {}
+file = open("pantadeusz.txt", "r", encoding = "utf-8")
+
+for line in file:
+    wordsList.extend(line.lower().split())
+
+file.close()
+
+for i in range(len(wordsList)):
+    wordsList[i] = wordsList[i].strip("\ufeff.!,()?:;/»«…")
+    
+    if len(wordsList[i]) >= 5:
+        if wordsList[i] in wordsDictionary.keys():
+            wordsDictionary[wordsList[i]] = wordsDictionary[wordsList[i]] + 1
+        else:
+            wordsDictionary[wordsList[i]] = 1
+
+wordsSorted = sorted(wordsDictionary, key = wordsDictionary.get, reverse = True)
+
+print("20 najczęściej występujących słów we wczytanym tekście:\n")
+for i in range (0, 20):
+    print('%i. "%s" - liczba wystąpień: %i' % (i + 1, wordsSorted[i], wordsDictionary[wordsSorted[i]]))
